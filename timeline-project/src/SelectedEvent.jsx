0ffe7db53d2a893d9e6c45d4d96f8b387cf92d3d
Fesenko Viktoria <vt221_fvv@student.ztu.edu.ua>
2024-12-27 15:27:20 +0000

    Додавання компоненту для відоюраження опису вибраних подій

---
+++ timeline-project/src/SelectedEvent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SelectedEvent = ({ event }) => {
+  if (!event) return <p>Please select an event to view details.</p>;
+
+  return (
+    <div>
+      <h2>{event.title}</h2>
+      <p>{event.date}</p>
+      <p>{event.description}</p>
+    </div>
+  );
+};
+
+export default SelectedEvent;
